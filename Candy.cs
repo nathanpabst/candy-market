41b7aadde23a4c6f0635f8ffb84c5fdf9ab31b29
Nathan Pabst <nathan.pabst@gmail.com>
2019-01-05 16:42:37 -0600

    added a list of candy types

--- Candy.cs
+++ Candy.cs
@@ -4,10 +4,11 @@ namespace candy_market
 {
     internal class Candy
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Name { get; set; }
         public string Manufacturer { get; set; }
-        public string FlavorCategory { get; set; }
+        public string Type { get; set; }
+        //public int Quantity { get; set; }
         public DateTime Date { get; set; }
     }
 } 
